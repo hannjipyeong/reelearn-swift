ca5835a7a73e602e6a2b100a314d3d41dff8496d
rehanzhaky <hannn9491@gmail.com>
2026-07-06 00:27:43 +0700

    feat: add print text learning material

---
+++ 03-Swift-Output/printText.swift
@@ -0,0 +1,19 @@
+//
+//  printText.swift
+//  
+//
+//  Created by Raihan Zhaky Al Hafizh on 06/07/26.
+//
+
+// print is used to write text and value to output
+// dont forget to use string interpolation to combine that values
+print("hello, Mars!")
+let name: String = "Raihan"
+print("hello, \(name)!")
+
+
+// print without newline
+// use "terminator" keyword
+for n in 1...5 {
+    print(n, terminator: " ")
+}
