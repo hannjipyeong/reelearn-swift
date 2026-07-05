3cd302b90ef9a20e8ce7beecf610a182ab6375d9
rehanzhaky <hannn9491@gmail.com>
2026-07-06 01:14:44 +0700

    feat: add print number learning material

---
+++ 03-Swift-Output/printNumber.swift
@@ -0,0 +1,33 @@
+//
+//  printNumber.swift
+//  
+//
+//  Created by Raihan Zhaky Al Hafizh on 06/07/26.
+//
+
+let kacangMedanPrice = 10000
+let pisangSalaiPrice = 17500
+
+// space separated
+print(kacangMedanPrice, pisangSalaiPrice)
+
+// interpolation
+print("makanan 1 = \(kacangMedanPrice), makanan 2 = \(pisangSalaiPrice)")
+
+// inline math
+print("Total yang harus dibayar adalah Rp\(kacangMedanPrice + pisangSalaiPrice)")
+
+
+// math function
+// functions that frequently appear in mathematic are abs, min, max
+let a = -10
+let b = 20
+
+// abs (absolute)
+print(abs(a))
+
+// min
+print(min(10, 3))
+
+// max
+print(max(a, b))
